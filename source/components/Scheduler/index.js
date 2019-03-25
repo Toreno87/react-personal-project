cc0f11b390a76a8c37230c48fbb64e5dde1687ac
Konstantin <tocet87@gmail.com>
2019-03-25 14:08:31 +0200

    add task component

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -5,11 +5,55 @@ import React, { Component } from 'react';
 import Styles from './styles.m.css';
 import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
 
+// Cpmponents
+import Task from '../Task';
+import Checkbox from '../../theme/assets/Checkbox';
+
 export default class Scheduler extends Component {
     render () {
+        const maxLengthInput = 50;
+        const complite = false;
+
         return (
             <section className = { Styles.scheduler }>
-                Планировщик: стартовая точка
+                <main>
+                    <header>
+                        <h1>
+                            Планировщик
+                        </h1>
+                        <input
+                            type = 'text'
+                            placeholder = 'Поиск по задачам'
+                        />
+                    </header>
+                    <section>
+                        <form action = ''>
+                            <input
+                                type = 'text'
+                                placeholder = 'Описание моей новой задачи (не 50 символов)'
+                                maxLength = { maxLengthInput }
+                            />
+                            <button type = 'submit'>
+                                Добавить задачу
+                            </button>
+                        </form>
+                        <ul>
+                            <Task />
+                        </ul>
+                    </section>
+                    <footer >
+                        <Checkbox
+                            inlineBlock
+                            checked = { complite }
+                            className = { Styles.toggleTaskCompletedState }
+                            color1 = '#323232'
+                            color2 = '#FFF'
+                        />
+                        <span className = { Styles.completeAllTasks } >
+                            Все задачи выполнены
+                        </span>
+                    </footer>
+                </main>
             </section>
         );
     }
--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -4,6 +4,12 @@ import React, { PureComponent } from 'react';
 // Instruments
 import Styles from './styles.m.css';
 
+// Components
+import Checkbox from '../../theme/assets/Checkbox';
+import Star from '../../theme/assets/Star';
+import Edit from '../../theme/assets/Edit';
+import Remove from '../../theme/assets/Remove';
+
 export default class Task extends PureComponent {
     _getTaskShape = ({
         id = this.props.id,
@@ -18,6 +24,47 @@ export default class Task extends PureComponent {
     });
 
     render () {
-        return <li className = { Styles.task }>Задача: стартовая точка</li>;
+        const maxLengthInput = 50;
+        const isDisabled = true;
+        const complite = false;
+
+        return (
+            <li className = { Styles.task }>
+                <div className = { Styles.content }>
+                    <Checkbox
+                        inlineBlock
+                        checked = { complite }
+                        className = { Styles.toggleTaskCompletedState }
+                        color1 = '#3B8EF3'
+                        color2 = '#FFF'
+                    />
+                    <input
+                        type = 'text'
+                        maxLength = { maxLengthInput }
+                        disabled = { isDisabled }
+                        value = 'тут будет какой-то текст...'
+                    />
+                </div>
+                <div className = { Styles.actions }>
+                    <Star
+                        inlineBlock
+                        className = { Styles.toggleTaskFavoriteState }
+                        color1 = '#3B8EF3'
+                        color2 = '#323232'
+                    />
+                    <Edit
+                        inlineBlock
+                        className = { Styles.updateTaskMessageOnClick }
+                        color1 = '#3B8EF3'
+                        color2 = '#323232'
+                    />
+                    <Remove
+                        inlineBlock
+                        color1 = '#3B8EF3'
+                        color2 = '#323232'
+                    />
+                </div>
+            </li>
+        );
     }
 }
